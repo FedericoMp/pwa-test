e1711eb1797bb6cc8d03a6f27934f2292d18f5e5
Fede <fede._perez@hotmail.com>
2018-05-25 16:42:40 -0300

    Test github pages and host firebase issues

---
+++ sw.js
@@ -0,0 +1,36 @@
+self.addEventListener('install', function (event) {
+  console.log('SW Installed');
+  event.waitUntil(
+    caches.open('static')
+      .then(function (cache) {
+        cache.addAll([
+          '/',
+          '/index.html',
+          '/js/',
+          '/css/',
+          '/images/',
+          '/mail/',
+          '/vendor/',
+          'https://fonts.googleapis.com/css?family=Montserrat:400,700',
+          'https://fonts.googleapis.com/css?family=Lato:400,700,400italic,700italic'
+        ]);
+      })
+  );
+});
+
+self.addEventListener('activate', function () {
+  console.log('SW Activated');
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(res) {
+        if (res) {
+          return res;
+        } else {
+          return fetch(event.request);
+        }
+      })
+  );
+});
